30f06804742029242c33849f6a4748d840049484
Tech-Priest Kirdos PC <kirillichz941@gmail.com>
2020-03-26 20:30:04 +0300

    Project created

---
+++ SimpleRunner/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject Player;
+    public int levelLength = 20;
+    public float levelPartLength = 20f;
+    private int[] pathPositionsZ = new int[3] { -5, 0, 5 };
+    private float startingX = 0f;
+    private float currentX;
+    private GameObject[] ActiveLevelParts;
+    private List<GameObject> ActiveGameObjects = new List<GameObject>();
+
+    void Start()
+    {
+        ActiveLevelParts = new GameObject[levelLength];
+        currentX = startingX;
+        for (int i = 0; i < levelLength; i++)
+        {
+            SpawnLevelPart(currentX);
+            SpawnLevelObjects();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ActiveLevelParts.Length; i++)
+        {
+            if (Player.transform.position.x - ActiveLevelParts[i].transform.position.x > 20f)
+            {
+                ActiveLevelParts[i].GetComponent<PoolObject>().ReturnToPool();
+                ActiveLevelParts[i] = null;
+                SpawnLevelPart(currentX);
+            }
+        }
+
+    }
+
+    void SpawnLevelPart(float x)
+    {
+        GameObject levelObject = PoolManager.GetObject("Level_part_01", new Vector3(currentX, 0, 0), Quaternion.identity);
+        for (int i = 0; i < ActiveLevelParts.Length; i++)
+        {
+            if (ActiveLevelParts[i] == null)
+            {
+                ActiveLevelParts[i] = levelObject;
+                break;
+            }
+        }
+        currentX += levelPartLength;
+    }
+    void SpawnLevelObjects()
+    {
+        
+    }
+}
---
+++ SimpleRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float forwardMoveSpeed = 20f;
+    public float sideMoveSpeed = 5f;
+    private bool StartGame = false;
+    private bool isChangingLane;
+    private float zToReach = 0f;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) StartGame = true;
+        if (StartGame)
+        {
+            if (isChangingLane)
+            {
+                if (transform.position.z != zToReach)
+                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, zToReach), sideMoveSpeed * Time.deltaTime);
+                else
+                    isChangingLane = false;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.D) && transform.position.z != -5)
+                {
+                    isChangingLane = true;
+                    zToReach -= 5f;
+                }
+                if (Input.GetKeyDown(KeyCode.A) && transform.position.z != 5)
+                {
+                    isChangingLane = true;
+                    zToReach += 5f;
+                }
+
+            }
+            transform.Translate(Vector3.right * forwardMoveSpeed * Time.deltaTime);
+        }
+
+    }
+}
